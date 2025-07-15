a2439c85af8196e1ee0c03914bec83635b7592f2
mateusribeirocampos <mateusrcampos@gmail.com>
2025-07-15 15:52:17 -0300

    Initial commit - project created

---
+++ src/main/java/com/campos/sbmongoDb/SpringBootCrudApiMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.campos.sbmongoDb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCrudApiMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCrudApiMongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/campos/sbmongoDb/SpringBootCrudApiMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.campos.sbmongoDb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCrudApiMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

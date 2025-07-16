b089b46a30c544ea5aa740cc413699141e4a26eb
mateusribeirocampos <mateusrcampos@gmail.com>
2025-07-16 16:49:49 -0300

    feat: Get user by ID and exceptions

--- src/main/java/com/campos/sbmongoDb/services/UserService.java
+++ src/main/java/com/campos/sbmongoDb/services/UserService.java
@@ -1,21 +1,28 @@
 package com.campos.sbmongoDb.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.campos.sbmongoDb.domain.User;
 import com.campos.sbmongoDb.repository.UserRepository;
+import com.campos.sbmongoDb.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repo;
-	
+
 	public List<User> findAll() {
 		return repo.findAll();
 	}
-	
+
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found!"));
+	}
+
 }
